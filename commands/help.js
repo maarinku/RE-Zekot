d870a5ca887e20780acbe6fce1541729b931cc81
maarinku <chiddinon@proton.me>
2022-11-28 10:40:04 +0900

    Half of .help

--- commands/help.js
+++ commands/help.js
@@ -6,6 +6,13 @@ module.exports = {
     permissions: null,
     deletable: false,
     async execute(message, args, client, info, functions) {
-        await message.channel.send("no.")
+        await message.channel.send({ embeds: [{
+            color: 0x9d58b6,
+            title: "Help",
+            description: "<a:ze_arrrow:1046585778895061072> *Select command category!* \n\n Get more information a bout the bot by executing the `.info` command.",
+            footer: {
+                text: "You can also use .help <command>"
+            }
+          }]});
     }
 }
